20c8e6335b8b9a73ed909c308e5ac72e7dcc7d01
SkyForgerUK <ulvis.andersons@gmail.com>
2022-10-02 10:07:02 +0000

    Add constants for DOM elements to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("initial commit")
+/**
+ * declare contants for all DOM elements
+ * and choices
+ */  
+
+
+const playerScore = document.getElementById('your-score');
+const computerScore = document.getElementById('opponent-score');
+const playerchoice = document.getElementById('your-card');
+const computerchoice = document.getElementById('opponent-card');
+const message = document.getElementsByClassName('interaction-text');
+const buttons = document.getElementsByClassName('button');
+const choices = [rock, paper, scissors];
+
+
+
+
