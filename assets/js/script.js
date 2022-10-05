0bbe6bab6fdfdce0d7638381cc1fee2d9c0bdb88
SkyForgerUK <ulvis.andersons@gmail.com>
2022-10-05 15:10:37 +0000

    Add js code for the pop-up button

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,6 @@ function playGame(playerMove) {
     } else if (choices[playerMove] === choices[computerMove]) {
         messageDraw();
     }
-
-
 }
 
 function updatePlayerScore() {
@@ -90,3 +88,45 @@ function messageDraw() {
     document.getElementById('interaction-text').innerText = `"It's a Draw"`;    
 }
 
+/**
+ * Pop-up button code
+ */
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]'); 
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+
+function openModal(modal) {
+    if (modal == null ) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null ) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
